d976f79878c5e67812bf3f7775c5cb7613c3fc93
alu0100851700 <alu0100851700@ull.edu.es>
2015-12-07 15:29:16 +0000

    Creadas expectativas ADABibliography

---
+++ spec/apabibliography.rb
@@ -0,0 +1,33 @@
+#encodign: utf-8
+require 'spec_helper'
+require 'biblio_gem'
+
+describe BiblioGem::ADABibliography do
+
+    describe "Almacenamiento de datos (1 autor):" do
+        before :each do
+            @bib1 = BiblioGem::ADABibliography.new(tipo: "Libro", nombres: ["Nombre1","Nombre2", "Nombre3"], apellidos: ["Apellido1", "Apellido2", "Apellido 3"], titulo: "Titulo", fecha: "Fecha", lugar: "Lugar1", isbn: "ISBN")
+            @bib2 = BiblioGem::ADABibliography.new(tipo: "Articulo",nombres: ["Nombre"], apellidos: ["Apellido"], titulo: "Titulo", fecha: "Fecha", lugar: "Lugar2", isbn: "ISBN")
+        end
+
+    it "La referencia contiene los nombres de los autores" do
+        expect(@bib1.nombres).to eq ["Nombre1", "Nombre2", "Nombre3"]
+        expect(@bib2.nombres).to eq ["Nombre"]
+    end
+
+    it "La referencia contiene los apellidos de los autores" do
+        expect(@bib1.apellidos).to eq ["Apellido1", "Apellido2", "Apellido3"]
+        expect(@bib2.apellidos).to eq ["Apellido"] 
+    end
+    
+     it "La referencia contiene un tipo" do
+        expect(@bib1.apellidos).to eq "Libro"
+        expect(@bib2.apellidos).to eq "Articulo"
+    end
+    
+    it "La referencia contiene el lugar de publicación" do
+        expect(@bib1.lugar).to eq "Lugar1"
+        expect(@bib2.lugar).to eq "Lugar2"
+    end
+   end
+end
